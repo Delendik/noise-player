ca6963e411bf8061d75ce9fe8721dbe4904dd31b
Alexey Delendik <volosya007@yandex.ru>
2021-08-13 23:15:42 +0300

    change white volume and octaves

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,46 @@ import './App.css';
 
 function App() {
 let volume:any = -20
+let volumeWhite:any = -30
 let player:any
 player = new Tone.Noise("white")
 const white = () =>{
   player.type="white"
+  player.volume.value = (volumeWhite);
 }
 const pink = () =>{
+  player.volume.value = (volume);
   player.type="pink"
 }
 const brown = () =>{
+  player.volume.value = (volume);
   player.type="brown"
 }
 
 const upVolume = () => {
   volume+=1
-  player.volume.value = (volume);
+  volumeWhite+=1
+  if(player.type === "white"){
+    player.volume.value = (volumeWhite);
+  }else{
+    player.volume.value = (volume);
+  }
 }
 const downVolume = () => {
   volume -=1
-  player.volume.value = (volume);
+  volumeWhite-=1
+  if(player.type === "white"){
+    player.volume.value = (volumeWhite);
+  }else{
+    player.volume.value = (volume);
+  }
 }
 
 const start = () =>{
+  console.log(player)
   if(player.type === "white"){
     player = new Tone.Noise("white").start();
-    player.volume.value = volume;
-    console.log(player)
+    player.volume.value = volumeWhite;
   }else if(player.type === "pink"){
     player = new Tone.Noise("pink").start();
     player.volume.value = volume;
@@ -39,10 +53,24 @@ const start = () =>{
   }
 
 // // make an autofilter to shape the noise
+// let autoFilter:any
+// if(player.type === "brown"){
+//   autoFilter = new Tone.AutoFilter({
+//     frequency: "0n",
+//     baseFrequency: 200,
+//     octaves: 4
+//   }).toDestination().start();
+// }else{
+//   autoFilter = new Tone.AutoFilter({
+//     frequency: "0n",
+//     baseFrequency: 200,
+//     octaves: 8
+//   }).toDestination().start();
+// }
 const autoFilter = new Tone.AutoFilter({
-	frequency: "0n",
-	baseFrequency: 200,
-	octaves: 8
+  frequency: "0n",
+  baseFrequency: 200,
+  octaves: 4
 }).toDestination().start();
 // connect the noise
 player.connect(autoFilter);
